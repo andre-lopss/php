d178bfcec5cd9fd67d1af55b4a9d085d04fbfebb
Andre Lopes <andrelneres@hotmail.com>
2022-03-22 20:11:21 -0300

    resolvido os problemas de atualização

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php 
-<<<<<<< HEAD
+
   echo "Olá, mundo 456!";
-  echo "Olá, Lopes!"
-=======
-  echo "Olá, mundo 123!";
-  echo "Olá, André!";
->>>>>>> 9eaf9cfabf8c258e728914b62932fcf645e6d3e7
+  echo "Olá, André Lopes!"
+
 ?>
